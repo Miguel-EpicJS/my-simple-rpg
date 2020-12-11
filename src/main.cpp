21014a32e0eb8382dfabc2b70334ec3313b6d224
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-11 18:20:41 -0300

    Create a battle

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include "BasicPerson/Enemy.hpp"
 #include "BasicPerson/Player.hpp"
+#include "Battle/Battle.hpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    Player miguel("Miguel", 120, 15, 1.3, 25, 0);
-    Enemy goblin("Goblin", 120,15,1.2,25,0);
+    Player *miguel = new Player("Miguel", 120, 15, 1.3, 25, 0);
+    Enemy *goblin = new Enemy("Goblin", 120,15,1.2,25,0);
 
-    cout << miguel.attack() << endl;
-    cout << goblin.attack() << endl;
+    Battle arena;
+    arena.start(miguel, goblin);
 
+    delete miguel, goblin;
     return 0;
 }
