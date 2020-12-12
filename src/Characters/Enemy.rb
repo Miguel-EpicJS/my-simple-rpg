4a2b53c2c56570aae6c87de758521ac3fe6195c6
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-12 18:11:15 -0300

    New attack for Enemies and Players

--- src/Characters/Enemy.rb
+++ src/Characters/Enemy.rb
@@ -1,7 +1,9 @@
 require_relative 'Characters'
+require_relative '../Dice/Dice'
 
 class Enemy < Characters
     def attack()
-        return @str * @speed
+        dice = Dice.new
+        return (@str * @speed).to_f * (dice.generate(20).to_f / 10) 
     end
 end
