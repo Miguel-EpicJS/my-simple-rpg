807bdf589a6d1215ff2d22496de93cbc57565561
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-14 09:49:47 -0300

    Simple game logic for search an enemy, recover hp and exit

--- src/Game/Loop.rb
+++ src/Game/Loop.rb
@@ -1,24 +1,38 @@
+require_relative '../Battle/Battle'
+require_relative '../Characters/Enemy'
 class Loop
     def initialize(player)
         @player = player
         #Make somethings here
         self.loop
     end
-    def loop
-        cmd = 0
-        puts "What you want to do"
-        puts "Type 1 to search an enemy"
-        puts "Type 2 to recover health"
-        puts "Type 3 to exit"
-        cmd = gets.chomp.to_i
-        if cmd == 1
-            puts "Searching..."
-            self.loop
-        elsif cmd == 2
-            puts "Healing..."
-            self.loop
-        elsif cmd == 3
-            puts "Exiting"
+    def logic
+        while true
+            cmd = 0
+            puts "What you want to do"
+            puts "Type 1 to search an enemy"
+            puts "Type 2 to recover health"
+            puts "Type 3 to exit"
+            cmd = gets.chomp.to_i
+            if cmd == 1
+                puts "Searching..."
+                sleep(0.2)
+                puts "Searching..."
+                sleep(2)
+                puts "We found a goblin!"
+                goblin = Enemy.new('Goblin', 10, 100, 1.1, 50)
+                battle = Battle.new
+                @player, goblin = battle.battle(@player, goblin)
+            elsif cmd == 2
+                puts "Healing..."
+                @player.hp += @player.healing
+                sleep(2)
+                puts "Your hp is #{@player.hp}"
+            elsif cmd == 3
+                puts "Exiting"
+                break
+            end
         end
+        return @player
     end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,9 +1,6 @@
-require_relative 'Characters/Enemy'
 require_relative 'Characters/Player'
-require_relative 'Battle/Battle'
+require_relative 'Game/Loop'
 
 miguel = Player.new('Miguel', 20, 120, 1.3, 50)
-goblin = Enemy.new('Goblin', 10, 100, 1.1, 50)
-battle = Battle.new
-
-miguel, goblin = battle.battle(miguel, goblin)
+logic = Loop.new(miguel)
+logic.logic
